c3a517ff518e3e040669c66df5e3f0983fedcdf6
Ludwin Burrión <LudwinBurrion@gmail.com>
2022-11-20 20:30:17 -0600

    Configuracion conexion y login

--- MeetDay.Infraestructura.Input.Web.Angular/ClientApp/src/app/components/auth/pages/login/login.component.ts
+++ MeetDay.Infraestructura.Input.Web.Angular/ClientApp/src/app/components/auth/pages/login/login.component.ts
@@ -1,36 +1,69 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  login : Boolean = true;
+  login: Boolean = true;
   loginForm!: FormGroup;
-  constructor(
-    private fb: FormBuilder,
-    private router: Router
-  ) { }
+  sigupForm!: FormGroup;
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      username: ['',Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+    this.sigupForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telephone: ['',Validators.required],
       password: ['',Validators.required]
+    });
+  }
+
+  onSubmitLogin() {
+    if (this.loginForm.valid) {
+    }
+    else{
+      this.validateAllFormFields(this.loginForm)
+    }
+  }
+
+  onSubmitSignUp() {
+    if(this.sigupForm.valid){
+
+    }
+    else{
+      this.validateAllFormFields(this.loginForm)
+    }
+  }
+
+  private validateAllFormFields(formGroup: FormGroup){
+    Object.keys(formGroup.controls).forEach(field=>{
+      const control = formGroup.get(field);
+      if(control instanceof FormControl){
+        control.markAsDirty({onlySelf: true});
+      }
+      else if(control instanceof FormGroup){
+        this.validateAllFormFields(control)
+      }
     })
   }
 
-  setSignUp(){
-    this.login = false
+  setSignUp() {
+    this.login = false;
   }
-  setSignIn(){
-    this.login = true
+  setSignIn() {
+    this.login = true;
   }
 
-  signIn(){
+  signIn() {
     this.router.navigate(['dashboard']);
   }
 }
